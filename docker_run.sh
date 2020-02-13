45525fa855b28cf8a357e7b460bd11d0b018713c
chumbleycode <chumbleycode@users.noreply.github.com>
2020-02-13 18:22:48 +0100

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -2,7 +2,7 @@
 
 docker run -it \
      -e PASSWORD=bioc \
-     -p 8787:8787  -v $(pwd):/home/rstudio/workspace \
+     -p 8787:8787  -v /Volumes/Share/projects/monkey:/home/rstudio/workspace \
      -v /Volumes/Share/preprocessed:/home/rstudio/workspace/data/preprocessed \
      -v /Volumes/share/projects/cell_type_cibersort:/home/rstudio/workspace/data/cell_type_cybersort \
      chumbleycode/monkey:0.0.19  bash
